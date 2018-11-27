b99693f74f32107e89d5ba5e7fe97c46b6cc14f8
Nicole D <nnardella@gmail.com>
2018-11-27 18:31:52 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
 
   def execute_transaction
-    binding.pry
+
     @receiver.balance = @receiver.balance + amount
     @sender.balance = @sender.balance - amount
     self.status = "complete"
